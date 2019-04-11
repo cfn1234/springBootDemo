86f917ca0ebcf120af0b3f0a1ba8a710761656a7
cfn1234 <435450916@qq.com>
2019-04-11 14:14:08 +0800

    提交spring boot初始类

---
+++ src/main/java/com/demo/BootApplication.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <br>
+ * 〈功能详细描述〉
+ * com.demo
+ *
+ * @author caofengnian 2019/4/11 0011 14:13
+ * @see [相关类/方法]（可选）
+ * @since [产品/模块版本] （可选）
+ */
+@SpringBootApplication
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+}
